3e0a8cbcd47bde11238316f8a2cbd91823d2828b
jaffasunny <jaffer.sunny125@gmail.com>
2021-11-06 14:43:10 +0500

    Person added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import "./App.css";
 
-const Person = () => {
-	const url = "https://randomuser.me/api/portraits/thumb/men/75.jpg";
+const Person = (props) => {
+	const { img, name, job } = props.person;
+	const { children } = props;
+	const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
 	return (
 		<div className="person">
 			<img src={url} alt="" />
 			<div>
-				<h4>name</h4>
-				<h4>job</h4>
+				<h4>{name}</h4>
+				<h4>{job}</h4>
 			</div>
 		</div>
 	);
 };
 
 const PersonList = () => {
-	return <Person></Person>;
+	const people = [
+		{
+			img: 22,
+			name: "John",
+			job: "Developer",
+		},
+		{
+			img: 24,
+			name: "Peter",
+			job: "Designer",
+		},
+		{
+			img: 30,
+			name: "Duckett",
+			job: "SQA",
+		},
+	];
+	return (
+		<section>
+			<Person person={people[0]} />
+			<Person person={people[1]} />
+			<Person person={people[2]} />
+		</section>
+	);
 };
 
 const App = () => {
